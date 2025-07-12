5fadd526f586eb56b18d40a27f0ecfb27965192c
Jeetsoni4447 <codecrafter4447@gmail.com>
2025-07-12 15:44:27 +0530

    Jk Tours Website Done

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-// import { createRoot } from "react-dom/client";
-// import { RouterProvider } from "react-router-dom";
-// import router from "./routes.js";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes.js";
 
-// createRoot(document.getElementById("root")).render(
-//   <RouterProvider router={router} />
-// );
+createRoot(document.getElementById("root")).render(
+    <RouterProvider router={router} />
+);
