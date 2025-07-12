271d0381c8e9b6dc843a60b9fc8f431a8079e744
Jeetsoni4447 <codecrafter4447@gmail.com>
2025-07-12 14:04:10 +0530

    Jk Tours Website Done

--- main.jsx
+++ main.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
+import { createRoot } from 'react-dom/client'
+
+import {
+  RouterProvider,
+} from "react-router";
 
 import router from './routes.js';
-import './index.css';
 
-const root = createRoot(document.getElementById('root'));
-<RouterProvider router={router} />
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+)
